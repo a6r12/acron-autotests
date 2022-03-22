d86b20968641131c18f046bd907806c5dbd1934e
akida <96776061+a6r12@users.noreply.github.com>
2022-03-22 10:50:53 +0300

    add version remote browser

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -14,7 +14,7 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {;
         Configuration.baseUrl = "https://www.acron.ru/";
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize =System.getProperty("size", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
 
         Configuration.browserVersion = System.getProperty("version", "91");
